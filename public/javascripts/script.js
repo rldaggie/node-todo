41aae2f3f771b9ca42ae0691d46f78571febdadd
Riley Dallas <rldaggie@gmail.com>
2016-07-29 15:35:32 -0500

    Add auth0 login

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,6 +1,19 @@
+var lock = new Auth0Lock('hEbMPdgCeruBufyJJiTKCkomgagGSs0g', 'trailerparkmedia.auth0.com', {
+    auth: {
+        params: { scope: 'openid email' }
+    }
+  }
+);
+
+$(document).on('click', '#login-cta', function (e) {
+  e.preventDefault();
+
+  lock.show();
+})
+
 $(document).on('click', '#todos-cta', function (e) {
   e.preventDefault();
-  
+
   $.ajax({
     url: '/api/v1/todos',
     method: 'GET'
